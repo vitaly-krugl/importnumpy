b8ae7af8bac686adbc7a36e697bf6ccb7c243d0d
Vitaly Kruglikov <vkruglikov@numenta.com>
2016-09-09 14:31:11 -0700

    Check if python is initialized to mimick behavior in PyRegion.cpp.

--- importnumpy.cpp
+++ importnumpy.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <iostream>
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <Python.h>
@@ -7,10 +8,17 @@
 int main(int argc, char*argv[])
 {
 
-Py_Initialize();
+  if (!Py_IsInitialized())
+  {
+    std::cerr << "ZZZ Calling Py_Initialize..." << std::endl;
+    Py_Initialize();
+    std::cerr << "ZZZ Returned from Py_Initialize..." << std::endl;
+    assert(Py_IsInitialized());
+  }
 
-std::cerr << "ZZZ Calling import_array1..." << std::endl;
-import_array1(-1);
-std::cerr << "ZZZ Returned from import_array1" << std::endl;
-
+  std::cerr << "ZZZ Calling import_array1..." << std::endl;
+  import_array1(-1);
+  std::cerr << "ZZZ Returned from import_array1" << std::endl;
+ 
+  return 0;
 }
